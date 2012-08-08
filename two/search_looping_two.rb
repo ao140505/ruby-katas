f15a1b10852de6b618c3325801d0d9838c13a3f9
Alex Okolish <alex.okolish@gmail.com>
2012-08-08 08:40:12 -0700

    this seems to work

--- two/search_looping_two.rb
+++ two/search_looping_two.rb
@@ -6,11 +6,10 @@ class SearchLoopingTwo
     right = col.count - 1
     middle = col.count / 2
 
-    #binding.pry if query == 5
     while right >= left do
       if query > col[middle]
         left = middle + 1
-        middle = (right - middle) / 2
+        middle = (right + left) / 2
       elsif query < col[middle]
         right = middle - 1
         middle = (middle - left) / 2
