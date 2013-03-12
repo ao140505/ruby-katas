a919f6f0681e1c9e2316907fc9850e05ea23ec40
Alex Okolish <alex.okolish@gmail.com>
2013-03-12 10:31:02 -0700

    quicksort wip

--- eleven/sort_chars.rb
+++ eleven/sort_chars.rb
@@ -1,5 +1,39 @@
-# start with something like this...
-"it's a Test".downcase.gsub(/[^a-z]+/, '')
+module Utils
+  class << self
+    def sort_chars(string)
+      chars = string.downcase.gsub(/[^a-z]+/, '').chars.to_a
+      chars = quicksort(chars)
+      chars.join
+    end
 
-# final answer should do this
-"it'''!s a Te,.st".downcase.gsub(/[^a-z]+/, '').chars.sort.join
+    private
+    def quicksort(chars)
+      length = chars.length
+      if length < 2 || chars.count(chars[0]) == length
+        return chars
+      end
+
+      pivot = chars[rand(length)]
+      lower, upper = chars.partition {|c| c < pivot}
+      quicksort(lower) + quicksort(upper)
+    end
+  end
+end
+
+require 'minitest/autorun'
+
+describe Utils do
+  describe ".sort_chars" do
+    it "sorts chars" do
+      #Utils.sort_chars("ao").must_equal 'ao'
+      #Utils.sort_chars("oa").must_equal 'ao'
+      Utils.sort_chars("test").must_equal 'estt'
+    end
+  end
+end
+
+# pick random pivot
+#
+# break into 2 arrays (partition step)
+# 1) els bigger than pivot
+# 2) els smaller than pivot
